2f9d835b0a45f81f472762049399a23ebb2ace7f
Jorge Alvarez <jorge@alvareznavarro.es>
2016-11-01 20:55:50 +0100

    Add bottom content to public template.

---
+++ imports/ui/public/pages/templates/home1/bottom_content.jsx
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import i18n from 'meteor/universe:i18n';
+
+import { PageContents } from '../../../../../../lib/collections/page_contents.js';
+
+import Loading from '../../../../utils/containers/loading.jsx';
+
+class PublicHome1BottomContent extends Component {
+  findBottomContent() {
+    return this.props.pageContents[0].text;
+  }
+
+  render() {
+    if (this.props.loading) {
+      return(<Loading />);
+    };
+
+    return(
+      <div dangerouslySetInnerHTML={ { __html: this.findBottomContent() } } ></div>
+    );
+  }
+}
+
+PublicHome1BottomContent.propTypes = {
+  pageId: PropTypes.string.isRequired
+};
+
+export default createContainer((props) => {
+  let subscription = Meteor.subscribe('pageContentsForType', props.pageId, 'bottom-content');
+
+  return {
+    loading: !subscription.ready(),
+    pageContents: PageContents.find({ contentType: 'bottom-content' }, { $sort: { order: 0 }}).fetch(),
+  };
+}, PublicHome1BottomContent);
--- imports/ui/public/pages/templates/home1/index.jsx
+++ imports/ui/public/pages/templates/home1/index.jsx
@@ -4,6 +4,7 @@ import i18n from 'meteor/universe:i18n';
 
 import PublicHome1Slider from './slider.jsx'
 import PublicHome1MainContent from './main_content.jsx'
+import PublicHome1BottomContent from './bottom_content.jsx'
 
 export default class PublicHome1Template extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class PublicHome1Template extends Component {
       <div className="wrapper">
         <PublicHome1Slider pageId={ this.props.page._id } />
         <PublicHome1MainContent pageId={ this.props.page._id } />
+        <PublicHome1BottomContent pageId={ this.props.page._id } />
       </div>
     );
   }
